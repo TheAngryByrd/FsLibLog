74cb3053caa7f2cf3eda24f2f6679adf331a6c59
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2019-06-21 16:26:45 -0400

    Adds Npgsql adapter

---
+++ adapters/FsLibLog.Adapters.Npgsql/FsLibLog.Adapters.Npgsql.fs
@@ -0,0 +1,49 @@
+namespace FsLibLog.Adapters.Npgsql
+
+open FsLibLog
+open FsLibLog.Types
+open Npgsql.Logging
+
+type FsLibLogLogger (logger :  FsLibLog.Types.ILog) =
+    inherit NpgsqlLogger() with
+
+        let mapLogLevels (level : NpgsqlLogLevel) =
+            match level with
+            | NpgsqlLogLevel.Trace -> LogLevel.Trace
+            | NpgsqlLogLevel.Debug -> LogLevel.Debug
+            | NpgsqlLogLevel.Info ->  LogLevel.Info
+            | NpgsqlLogLevel.Warn ->  LogLevel.Warn
+            | NpgsqlLogLevel.Error -> LogLevel.Error
+            | NpgsqlLogLevel.Fatal -> LogLevel.Fatal
+            | _ -> LogLevel.Trace
+
+        override __.IsEnabled(level : NpgsqlLogLevel) =
+            level
+            |> mapLogLevels
+            |> Log.StartLogLevel
+            |> logger.fromLog
+        override __.Log
+            (   level : NpgsqlLogLevel,
+                connectorId : int,
+                msg : string,
+                ex : exn) =
+            let log =
+                level
+                |> mapLogLevels
+                |> Log.StartLogLevel
+            let message = sprintf "{connectorId} : %s" msg
+            let logConfig =
+                Log.setMessage message
+                >> Log.addParameter connectorId
+                >> Log.addException ex
+            log
+            |> logConfig
+            |> logger.fromLog
+            |> ignore
+
+type FsLibLogLoggerProvider () =
+    interface INpgsqlLoggingProvider with
+        member __.CreateLogger (name : string) =
+            LogProvider.getLoggerByName name
+            |> FsLibLogLogger
+            :> NpgsqlLogger
